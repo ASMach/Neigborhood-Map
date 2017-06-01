2e49c9d9720ee9b0d3f7ac2c0e9380d69053aacd
Alonzo Machiraju <machthree@gmail.com>
2017-06-01 15:42:27 -0700

    Attempted to fix polygon search, but there is another problem. superfluous code has also been removed

--- scripts/mapscript.js
+++ scripts/mapscript.js
@@ -218,7 +218,7 @@ function MapDataModel(title)
     // This function hides all markers outside the polygon,
     // and shows only the ones within it. This is so that the
     // user can specify an exact area of search.
-    function searchWithinPolygon() {
+    function searchWithinPolygon(polygon) {
         for (var i = 0; i < self.markers().length; i++) {
             if (google.maps.geometry.poly.containsLocation(self.markers()[i].position, polygon)) {
                 self.markers()[i].setMap(map);
@@ -227,36 +227,6 @@ function MapDataModel(title)
             }
         }
     }
-    
-    // This function takes the input value in the find nearby area text input
-    // locates it, and then zooms into that area. This is so that the user can
-    // show all listings, then decide to focus on one area of the map.
-    function zoomToArea() {
-        // Initialize the geocoder.
-        var geocoder = new google.maps.Geocoder();
-        // Get the address or place that the user entered.
-        var address = document.getElementById('zoom-to-area-text').value;
-        // Make sure the address isn't blank.
-        if (address == '') {
-            window.alert('You must enter an area, or address.');
-        } else {
-            // Geocode the address/area entered to get the center. Then, center the map
-            // on it and zoom in
-            geocoder.geocode(
-                             { address: address,
-                             componentRestrictions: {locality: 'New York'}
-                             }, function(results, status) {
-                             if (status == google.maps.GeocoderStatus.OK) {
-                             map.setCenter(results[0].geometry.location);
-                             map.setZoom(15);
-                             } else {
-                             window.alert('We could not find that location - try entering a more' +
-                                          ' specific place.');
-                             }
-                             });
-        }
-    }
-    
     
     // This function allows the user to input a desired travel time, in
     // minutes, and a travel mode, and a location - and only show the listings
