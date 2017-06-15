9fbeb0b402c8af9eced067417c339374010b0f5e
Alonzo Machiraju <machthree@gmail.com>
2017-06-15 12:08:14 -0700

    ShowInfoWindow moved to knockout model

--- scripts/mapscript.js
+++ scripts/mapscript.js
@@ -211,6 +211,17 @@ function MapDataModel(title)
         }
     };
     
+
+    // Highlights a marker when its associated address is clicked
+    self.showMarkerForAddress = function showMarkerForAddress()
+    {
+        console.log("TODO: Take the title find the appropriate marker, and enable it");
+     
+        //self.markers()[i].setMap(map);
+        //bounds.extend(self.markers()[i].position);
+     
+    };
+
     // This function will go through each of the results, and,
     // if the distance is LESS than the value in the picker, show it on the map.
     function displayMarkersWithinTime(response) {
@@ -415,6 +426,102 @@ function MapDataModel(title)
             self.markers()[i].setMap(null);
         }
     };
+    
+    // This function populates the infowindow when the marker is clicked. We'll only allow
+    // one infowindow which will open at the marker that is clicked, and populate based
+    // on that markers position.
+    self.populateInfoWindow = function populateInfoWindow(marker, infowindow) {
+        
+        // In case the status is OK, which means the pano was found, compute the
+        // position of the streetview image, then calculate the heading, then get a
+        // panorama from that and set the options
+        function getStreetView(data, status) {
+            if (status == google.maps.StreetViewStatus.OK) {
+                var nearStreetViewLocation = data.location.latLng;
+                var heading = google.maps.geometry.spherical.computeHeading(
+                                                                            nearStreetViewLocation, marker.position);
+                infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>' + foursqareDiv);
+                var panoramaOptions = {
+                position: nearStreetViewLocation,
+                pov: {
+                heading: heading,
+                pitch: 30
+                }
+                };
+                var panorama = new google.maps.StreetViewPanorama(
+                                                                  document.getElementById('pano'), panoramaOptions);
+            } else {
+                infowindow.setContent('<div>' + marker.title + '</div>' +
+                                      '<div>No Street View Found</div>' + foursqareDiv);
+            }
+        }
+        
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+            // Clear the infowindow content to give the streetview time to load.
+            infowindow.setContent('');
+            infowindow.marker = marker;
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener('closeclick', function() {
+                                   infowindow.marker = null;
+                                   });
+            var streetViewService = new google.maps.StreetViewService();
+            var radius = 50;
+            
+            // Foursquare setup
+            
+            var foursqareDiv;
+            
+            var apiURL = 'https://api.foursquare.com/v2/venues/search?ll=';
+            var foursquareClientID = 'PTVTCDT10DK4VAWWNNLCPVFQPFJQ3DQVKNASSAVFNJSR0JFH';
+            var foursquareSecret ='11Z1QM04KPUZTF1CZVSS0Z4SRZNFDFYOBI2G0BY3RKYKG4WM';
+            var foursquareVersion = '20170112';
+            
+            var foursquareLocation = marker.getPosition().lat() + ',' + marker.getPosition().lng();
+            
+            var foursquareURL = apiURL + foursquareLocation + '&client_id=' + foursquareClientID +  '&client_secret=' + foursquareSecret +'&v=' + foursquareVersion;
+            
+            
+            $.ajax({
+                   type: "GET",
+                   url: foursquareURL,
+                   dataType: "json",
+                   success: function (data) {
+                   
+                   // Parse our response data and then extract the venues for iteration
+                   
+                   var venues = data.response.venues;
+                   
+                   foursqareDiv = '<div>Nearby Schools:</div>';
+                   
+                   
+                   venues.forEach(function(venue){
+                                  var name = venue.name;
+                                  
+                                  // We don't want to append a blank space!
+                                  
+                                  if (name !== null && name.includes("School")) foursqareDiv = foursqareDiv + '<div>' + name + '</div>';
+                                  
+                                  });
+                   
+                   // Handle street view only after we have our FourSquare data
+                   
+                   // Use streetview service to get the closest streetview image within
+                   // 50 meters of the markers position
+                   streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+                   
+                   },
+                   error: function (data) {
+                   window.alert('FourSquare error was: ' + data.status + ' ' + data.statusText);
+                   foursqareDiv = '<div>Cannot access FourSquare API</div>';
+                   }
+                   });
+            
+            
+            // Open the infowindow on the correct marker.
+            infowindow.open(map, marker);
+        }
+    }
 }
 
 // Create a reference for our knockout view
@@ -432,102 +539,6 @@ function toggleBounce() {
 }
 
 // Google Maps drawing functions
-
-// This function populates the infowindow when the marker is clicked. We'll only allow
-// one infowindow which will open at the marker that is clicked, and populate based
-// on that markers position.
-function populateInfoWindow(marker, infowindow) {
-    
-    // In case the status is OK, which means the pano was found, compute the
-    // position of the streetview image, then calculate the heading, then get a
-    // panorama from that and set the options
-    function getStreetView(data, status) {
-        if (status == google.maps.StreetViewStatus.OK) {
-            var nearStreetViewLocation = data.location.latLng;
-            var heading = google.maps.geometry.spherical.computeHeading(
-                                                                        nearStreetViewLocation, marker.position);
-            infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>' + foursqareDiv);
-            var panoramaOptions = {
-            position: nearStreetViewLocation,
-            pov: {
-                    heading: heading,
-                    pitch: 30
-                }
-            };
-            var panorama = new google.maps.StreetViewPanorama(
-                                                              document.getElementById('pano'), panoramaOptions);
-        } else {
-            infowindow.setContent('<div>' + marker.title + '</div>' +
-                                  '<div>No Street View Found</div>' + foursqareDiv);
-        }
-    }
-    
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-        // Clear the infowindow content to give the streetview time to load.
-        infowindow.setContent('');
-        infowindow.marker = marker;
-        // Make sure the marker property is cleared if the infowindow is closed.
-        infowindow.addListener('closeclick', function() {
-                               infowindow.marker = null;
-                               });
-        var streetViewService = new google.maps.StreetViewService();
-        var radius = 50;
-        
-        // Foursquare setup
-        
-        var foursqareDiv;
-        
-        var apiURL = 'https://api.foursquare.com/v2/venues/search?ll=';
-        var foursquareClientID = 'PTVTCDT10DK4VAWWNNLCPVFQPFJQ3DQVKNASSAVFNJSR0JFH';
-        var foursquareSecret ='11Z1QM04KPUZTF1CZVSS0Z4SRZNFDFYOBI2G0BY3RKYKG4WM';
-        var foursquareVersion = '20170112';
-        
-        var foursquareLocation = marker.getPosition().lat() + ',' + marker.getPosition().lng();
-        
-        var foursquareURL = apiURL + foursquareLocation + '&client_id=' + foursquareClientID +  '&client_secret=' + foursquareSecret +'&v=' + foursquareVersion;
-        
-        
-        $.ajax({
-               type: "GET",
-               url: foursquareURL,
-               dataType: "json",
-               success: function (data) {
-               
-               // Parse our response data and then extract the venues for iteration
-               
-               var venues = data.response.venues;
-               
-               foursqareDiv = '<div>Nearby Schools:</div>';
-               
-               
-               venues.forEach(function(venue){
-                    var name = venue.name;
-                              
-                    // We don't want to append a blank space!
-                            
-                    if (name !== null && name.includes("School")) foursqareDiv = foursqareDiv + '<div>' + name + '</div>';
-
-                });
-               
-               // Handle street view only after we have our FourSquare data
-               
-               // Use streetview service to get the closest streetview image within
-               // 50 meters of the markers position
-               streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-               
-               },
-               error: function (data) {
-               window.alert('FourSquare error was: ' + data.status + ' ' + data.statusText);
-               foursqareDiv = '<div>Cannot access FourSquare API</div>';
-               }
-               });
-
-        
-        // Open the infowindow on the correct marker.
-        infowindow.open(map, marker);
-    }
-}
 
 // This function hides all markers outside the polygon,
 // and shows only the ones within it. This is so that the
@@ -548,7 +559,7 @@ function initMap() {
     
     function populateInfoWindowListener()
     {
-        populateInfoWindow(this, largeInfowindow);
+        mapView.populateInfoWindow(this, largeInfowindow);
     }
     
     function mouseoverListener()
